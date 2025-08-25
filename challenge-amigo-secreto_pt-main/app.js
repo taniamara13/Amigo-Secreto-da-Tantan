04d2001c84d2a4b9f2c7614e24856b7ab5e0c828
taniamara13 <tania.andrade@gmail.com>
2025-08-25 14:53:49 -0300

    Add friend management functionality and update styles for buttons and borders

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,3 +1,29 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let listaDeAmigos = [];
+
+function adicionarAmigo() {
+    let nomeDoAmigo = document.getElementById("amigo").value;
+    if (nomeDoAmigo === "") {
+        alert("Por favor, insira um nome.");
+        return;       
+    }
+    if (listaDeAmigos.includes(nomeDoAmigo)) {
+        alert("Este amigo já foi adicionado.");
+        return;
+    }
+    listaDeAmigos.push(nomeDoAmigo);
+    document.getElementById("amigo").value = "";
+    atualizarListaDeAmigos();
+
+    console.log(listaDeAmigos);
+}
+function atualizarListaDeAmigos() {
+    let listaHTML = document.getElementById("listaAmigos");
+    listaHTML.innerHTML = "";
+    for (let amigo of listaDeAmigos) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaHTML.appendChild(li);
+    }
+}
